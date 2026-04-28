9d72dbb25451a674da8bfcadf4f36bf6949250b8
Mahmoud Aladwy <mahmoudaladwy774@gmail.com>
2026-04-28 12:26:27 -0700

    - added files for project structure

---
+++ Sport Mob/Features/LeagueDetailsScreen/leagDetails.swift
@@ -0,0 +1,7 @@
+//
+//  leagDetails.swift
+//  Sport Mob
+//
+//  Created by Al3dwy on 28/04/2026.
+//
+
---
+++ Sport Mob/Model/Services/coreData.swift
@@ -0,0 +1,7 @@
+//
+//  coreData.swift
+//  Sport Mob
+//
+//  Created by Al3dwy on 28/04/2026.
+//
+
