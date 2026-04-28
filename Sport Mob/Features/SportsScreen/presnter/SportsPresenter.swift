3dd6541edb1c903666e8da4299e5ea8acc98e398
Ehab Salah <e.salah82055@gmail.com>
2026-04-28 20:18:30 +0300

    - Add all app screens in main storyboard - Add all view controllers and attach them with main storyboard screen - Complete the MVP folder structure

---
+++ Sport Mob/Features/SportsScreen/presnter/SportsPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  SportsPresenter.swift
+//  Sport Mob
+//
+//  Created by Ehab Salah on 28/04/2026.
+//
+
+import Foundation
+
+protocol SportsPresenter {
+}
+
+class SportsPresenterImp: SportsPresenter {
+}
