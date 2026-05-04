efa7e7bab35602e8fcd1f45c1dd0d20fcf62599c
Ehab Salah <e.salah82055@gmail.com>
2026-05-04 19:48:11 +0300

    Create SportDataModel
    - Add the two files of LeagueEntity manually
    - Add the attributes of LeagueEntity
    - make an constraints for id attribute

---
+++ LeagueEntity+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  LeagueEntity+CoreDataProperties.swift
+//  Sport Mob
+//
+//  Created by Ehab Salah on 04/05/2026.
+//
+//
+
+public import Foundation
+public import CoreData
+
+
+public typealias LeagueEntityCoreDataPropertiesSet = NSSet
+
+extension LeagueEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<LeagueEntity> {
+        return NSFetchRequest<LeagueEntity>(entityName: "LeagueEntity")
+    }
+
+    @NSManaged public var countryImage: String?
+    @NSManaged public var countryName: String?
+    @NSManaged public var id: Int64
+    @NSManaged public var leagueImage: String?
+    @NSManaged public var leagueName: String?
+
+}
+
+extension LeagueEntity : Identifiable {
+
+}
